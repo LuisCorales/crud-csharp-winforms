269983713cfc3e38ab13b8be2e67511403270625
Luis Corales <luisaugcorales@gmail.com>
2021-03-26 20:21:03 -0500

    Base crud done

---
+++ CrudWinForms/DataLayer/DL_Clients.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataLayer;
+using MySql.Data.MySqlClient;
+
+namespace DataLayer
+{
+    public class DL_Clients
+    {
+        string dbTable = "clients";     //Example "table"
+
+        DL_Connection dbConnection = new DL_Connection();
+
+        MySqlDataAdapter adapter;
+        DataTable table = new DataTable();
+        
+        //Retrieve the data of the DB to store it in a DataTable
+        public DataTable Show()
+        {
+            try
+            {
+                //Get the data in the selected DB
+                adapter = new MySqlDataAdapter("SELECT * FROM " + dbTable, dbConnection.DBConnection);
+
+                dbConnection.OpenConnection();
+
+                //Add the data to the DataTable
+                DataSet ds = new DataSet();
+                adapter.Fill(ds, dbTable);
+                table = ds.Tables[dbTable];
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                dbConnection.CloseConnection();
+            }
+
+            return table;
+        }
+
+        //Inserts a new row in the DB with all the data provided
+        public string Insert(string firstName, string lastName, string email, string phone, string idDocument)
+        {
+            string cmd = "INSERT INTO " + dbTable + " (firstName, lastName, email, phone, idDocument) VALUES ('"
+                    + firstName + "','" + lastName + "','" + email + "','" + phone + "','" + idDocument + "')";
+            
+            try
+            {
+                dbConnection.OpenConnection();
+
+                MySqlCommand command = new MySqlCommand(cmd, dbConnection.DBConnection);
+                command.ExecuteNonQuery();
+
+                return "Inserted succesfully!";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            finally
+            {
+                dbConnection.CloseConnection();
+            }
+        }
+
+        //Updates a row in the DB with all the data changes provided
+        public string Update(string id, string firstName, string lastName, string email, string phone, string idDocument)
+        {
+            string cmd = "UPDATE " + dbTable + " " +
+                "SET " +
+                    "firstName='" + firstName + "', " +
+                    "lastName='" + lastName + "', " +
+                    "email='" + email + "', " +
+                    "phone='" + phone + "', " +
+                    "idDocument='" + idDocument + "' " +
+                "WHERE id='" + id + "'";
+
+            try
+            {
+                dbConnection.OpenConnection();
+
+                MySqlCommand command = new MySqlCommand(cmd, dbConnection.DBConnection);
+                command.ExecuteNonQuery();
+
+                return "Updated succesfully!";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            finally
+            {
+                dbConnection.CloseConnection();
+            }
+        }
+
+        //Deletes a row in the DB by just providing the id
+        public string Delete(string id)
+        {
+            string cmd = "DELETE FROM " + dbTable + " WHERE id=" + id;
+
+            try
+            {
+                dbConnection.OpenConnection();
+
+                MySqlCommand command = new MySqlCommand(cmd, dbConnection.DBConnection);
+                command.ExecuteNonQuery();
+
+                return "Deleted succesfully!";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            finally
+            {
+                dbConnection.CloseConnection();
+            }
+        }
+    }
+}
---
+++ CrudWinForms/PresentationLayer/Form1.Designer.cs
@@ -0,0 +1,384 @@
+﻿
+namespace PresentationLayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.cbSearchFilters = new System.Windows.Forms.ComboBox();
+            this.txtbSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.dataGridViewDBTable = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtbIdDocument = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbPhone = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtbEmail = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtbLastName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtbFirstName = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lbTextFields = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDBTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.Gray;
+            this.splitContainer1.Panel1.Controls.Add(this.btnRefresh);
+            this.splitContainer1.Panel1.Controls.Add(this.btnSearch);
+            this.splitContainer1.Panel1.Controls.Add(this.txtbSearch);
+            this.splitContainer1.Panel1.Controls.Add(this.cbSearchFilters);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(1046, 504);
+            this.splitContainer1.SplitterDistance = 58;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // cbSearchFilters
+            // 
+            this.cbSearchFilters.FormattingEnabled = true;
+            this.cbSearchFilters.Location = new System.Drawing.Point(12, 19);
+            this.cbSearchFilters.Name = "cbSearchFilters";
+            this.cbSearchFilters.Size = new System.Drawing.Size(121, 21);
+            this.cbSearchFilters.TabIndex = 0;
+            // 
+            // txtbSearch
+            // 
+            this.txtbSearch.Location = new System.Drawing.Point(139, 20);
+            this.txtbSearch.Name = "txtbSearch";
+            this.txtbSearch.Size = new System.Drawing.Size(247, 20);
+            this.txtbSearch.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(392, 18);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(959, 17);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 5;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.dataGridViewDBTable);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.splitContainer3);
+            this.splitContainer2.Size = new System.Drawing.Size(1046, 442);
+            this.splitContainer2.SplitterDistance = 693;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.IsSplitterFixed = true;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            this.splitContainer3.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.lbTextFields);
+            this.splitContainer3.Panel1.Controls.Add(this.txtbFirstName);
+            this.splitContainer3.Panel1.Controls.Add(this.btnSave);
+            this.splitContainer3.Panel1.Controls.Add(this.label5);
+            this.splitContainer3.Panel1.Controls.Add(this.txtbLastName);
+            this.splitContainer3.Panel1.Controls.Add(this.label4);
+            this.splitContainer3.Panel1.Controls.Add(this.label1);
+            this.splitContainer3.Panel1.Controls.Add(this.txtbEmail);
+            this.splitContainer3.Panel1.Controls.Add(this.txtbIdDocument);
+            this.splitContainer3.Panel1.Controls.Add(this.label3);
+            this.splitContainer3.Panel1.Controls.Add(this.label2);
+            this.splitContainer3.Panel1.Controls.Add(this.txtbPhone);
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.splitContainer3.Panel2.Controls.Add(this.btnClear);
+            this.splitContainer3.Panel2.Controls.Add(this.btnEdit);
+            this.splitContainer3.Panel2.Controls.Add(this.btnDelete);
+            this.splitContainer3.Size = new System.Drawing.Size(349, 442);
+            this.splitContainer3.SplitterDistance = 363;
+            this.splitContainer3.TabIndex = 0;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(120, 28);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 29;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(262, 232);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 21;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(21, 28);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 28;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // dataGridViewDBTable
+            // 
+            this.dataGridViewDBTable.AllowUserToAddRows = false;
+            this.dataGridViewDBTable.AllowUserToDeleteRows = false;
+            this.dataGridViewDBTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewDBTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewDBTable.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewDBTable.MultiSelect = false;
+            this.dataGridViewDBTable.Name = "dataGridViewDBTable";
+            this.dataGridViewDBTable.ReadOnly = true;
+            this.dataGridViewDBTable.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewDBTable.Size = new System.Drawing.Size(693, 442);
+            this.dataGridViewDBTable.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(18, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "First Name";
+            // 
+            // txtbIdDocument
+            // 
+            this.txtbIdDocument.Location = new System.Drawing.Point(120, 192);
+            this.txtbIdDocument.Name = "txtbIdDocument";
+            this.txtbIdDocument.Size = new System.Drawing.Size(217, 20);
+            this.txtbIdDocument.TabIndex = 26;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(18, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Last Name";
+            // 
+            // txtbPhone
+            // 
+            this.txtbPhone.Location = new System.Drawing.Point(120, 155);
+            this.txtbPhone.Name = "txtbPhone";
+            this.txtbPhone.Size = new System.Drawing.Size(217, 20);
+            this.txtbPhone.TabIndex = 25;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(18, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Email";
+            // 
+            // txtbEmail
+            // 
+            this.txtbEmail.Location = new System.Drawing.Point(120, 118);
+            this.txtbEmail.Name = "txtbEmail";
+            this.txtbEmail.Size = new System.Drawing.Size(217, 20);
+            this.txtbEmail.TabIndex = 24;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(18, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Phone";
+            // 
+            // txtbLastName
+            // 
+            this.txtbLastName.Location = new System.Drawing.Point(120, 85);
+            this.txtbLastName.Name = "txtbLastName";
+            this.txtbLastName.Size = new System.Drawing.Size(217, 20);
+            this.txtbLastName.TabIndex = 23;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(18, 195);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "ID Document";
+            // 
+            // txtbFirstName
+            // 
+            this.txtbFirstName.Location = new System.Drawing.Point(120, 53);
+            this.txtbFirstName.Name = "txtbFirstName";
+            this.txtbFirstName.Size = new System.Drawing.Size(217, 20);
+            this.txtbFirstName.TabIndex = 22;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(262, 28);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 30;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lbTextFields
+            // 
+            this.lbTextFields.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTextFields.ForeColor = System.Drawing.Color.White;
+            this.lbTextFields.Location = new System.Drawing.Point(0, 10);
+            this.lbTextFields.Name = "lbTextFields";
+            this.lbTextFields.Size = new System.Drawing.Size(337, 24);
+            this.lbTextFields.TabIndex = 27;
+            this.lbTextFields.Text = "Insert New Data";
+            this.lbTextFields.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1046, 504);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel1.PerformLayout();
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDBTable)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtbSearch;
+        private System.Windows.Forms.ComboBox cbSearchFilters;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.DataGridView dataGridViewDBTable;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.TextBox txtbFirstName;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtbLastName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtbEmail;
+        private System.Windows.Forms.TextBox txtbIdDocument;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtbPhone;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lbTextFields;
+    }
+}
+
